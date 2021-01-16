0049e78e64a5f11d9ebc46ab21a62c4464712ba5
Gill <ishida624@gmail.com>
2021-01-16 17:57:50 +0800

    add command, improt books data to DB

--- app/Console/Commands/ImportJsonFile.php
+++ app/Console/Commands/ImportJsonFile.php
@@ -5,6 +5,8 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Models\User;
 use App\Models\PurchaseHistory;
+use App\Models\BookStore;
+use App\Models\Books;
 
 class ImportJsonFile extends Command
 {
@@ -42,43 +44,87 @@ class ImportJsonFile extends Command
 
         $action = $this->argument('action');
         $path = $this->option('path');
-        $userDataJsonFile = json_decode(file_get_contents($path), true);
-        foreach ($userDataJsonFile as  $userData) {
-            # 尋找資料庫內有無一樣的資料，沒有就新建立，有就更新
-            $userDataDB = User::where('name', $userData['name'])->first();
-            if (!$userDataDB) {
-                User::create([
-                    'cashBalance' => $userData['cashBalance'],
-                    'name' => $userData['name'],
-                ]);
-            } else {
-                $userDataDB->update([
-                    'cashBalance' => $userData['cashBalance'],
-                    'name' => $userData['name'],
-                ]);
-            }
-            foreach ($userData['purchaseHistory'] as  $purchaseHistory) {
-                $purchaseHistoryDB = PurchaseHistory::where('bookName', $purchaseHistory['bookName'])->first();
-                if (!$purchaseHistoryDB) {
-                    PurchaseHistory::create([
-                        'bookName' => $purchaseHistory['bookName'],
-                        'storeName' => $purchaseHistory['storeName'],
-                        'transactionAmount' => $purchaseHistory['transactionAmount'],
-                        'transactionDate' => $purchaseHistory['transactionDate'],
-                        'user_id' => $userDataDB->id,
+
+        $JsonFile = json_decode(file_get_contents($path), true);
+
+        if ($action === 'import_data:user') {      #判斷指令的action是什麼
+
+            foreach ($JsonFile as  $userData) {
+                # 尋找資料庫內有無一樣的資料，沒有就新建立，有就更新
+                $userDataDB = User::where('name', $userData['name'])->first();
+                if (!$userDataDB) {
+                    User::create([
+                        'cashBalance' => $userData['cashBalance'],
+                        'name' => $userData['name'],
                     ]);
                 } else {
-                    $purchaseHistoryDB->update([
-                        'bookName' => $purchaseHistory['bookName'],
-                        'storeName' => $purchaseHistory['storeName'],
-                        'transactionAmount' => $purchaseHistory['transactionAmount'],
-                        'transactionDate' => $purchaseHistory['transactionDate'],
-                        'user_id' => $userDataDB->id,
+                    $userDataDB->update([
+                        'cashBalance' => $userData['cashBalance'],
+                        'name' => $userData['name'],
                     ]);
                 }
+
+                $userDataDB = User::where('name', $userData['name'])->first();
+                foreach ($userData['purchaseHistory'] as  $purchaseHistory) {
+                    $purchaseHistoryDB = PurchaseHistory::where('bookName', $purchaseHistory['bookName'])->first();
+                    if (!$purchaseHistoryDB) {
+                        PurchaseHistory::create([
+                            'bookName' => $purchaseHistory['bookName'],
+                            'storeName' => $purchaseHistory['storeName'],
+                            'transactionAmount' => $purchaseHistory['transactionAmount'],
+                            'transactionDate' => $purchaseHistory['transactionDate'],
+                            'user_id' => $userDataDB->id,
+                        ]);
+                    } else {
+                        $purchaseHistoryDB->update([
+                            'bookName' => $purchaseHistory['bookName'],
+                            'storeName' => $purchaseHistory['storeName'],
+                            'transactionAmount' => $purchaseHistory['transactionAmount'],
+                            'transactionDate' => $purchaseHistory['transactionDate'],
+                            'user_id' => $userDataDB->id,
+                        ]);
+                    }
+                }
             }
+            // echo ('input user data');
+        } elseif ($action === 'import_data:book_store') {
+            foreach ($JsonFile as $bookStores) {
+                # code...
+                $bookStroeDB = BookStore::where('storeName', $bookStores['storeName'])->first();
+                if (!$bookStroeDB) {
+                    BookStore::create([
+                        'storeName' => $bookStores['storeName'],
+                        'openingHours' => $bookStores['openingHours'],
+                        'cashBalance' => $bookStores['cashBalance'],
+                    ]);
+                } else {
+                    $bookStroeDB->update([
+                        'storeName' => $bookStores['storeName'],
+                        'openingHours' => $bookStores['openingHours'],
+                        'cashBalance' => $bookStores['cashBalance'],
+                    ]);
+                }
+
+                $bookStroeDB = BookStore::where('storeName', $bookStores['storeName'])->first();
+                foreach ($bookStores['books'] as $book) {
+                    $bookDB = Books::where('bookName', $book['bookName'])->first();
+                    if (!$bookDB) {
+                        Books::create([
+                            'bookName' => $book['bookName'],
+                            'price' => $book['price'],
+                            'bookStore_id' => $bookStroeDB->id,
+                        ]);
+                    } else {
+                        $bookDB->update([
+                            'bookName' => $book['bookName'],
+                            'price' => $book['price'],
+                            'bookStore_id' => $bookStroeDB->id,
+                        ]);
+                    }
+                }
+            }
+        } else {
+            $this->info('action is wrong');
         }
-        // dd($userDataJsonFile[0]['purchaseHistory'][0]);
-        // return 0;
     }
 }
