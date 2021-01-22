4bc455dcbe2102af18008ba93da2a5712d4432d5
Gill <ishida624@gmail.com>
2021-01-22 15:46:12 +0800

    add import openingHours code

--- app/ImportBookStoreService.php
+++ app/ImportBookStoreService.php
@@ -4,15 +4,15 @@ namespace App;
 
 use App\Models\Books;
 use App\Models\BookStore;
+use App\Models\BookStoreOpenTime;
 
 class ImportBookStoreService
 {
-
+    public $weekArray = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
     public function importBookStore($JsonFile)
     {
-        # code...
+        $weekArray = $this->weekArray;
         foreach ($JsonFile as $bookStores) {
-            # code...
             $bookStroeDB = BookStore::where('storeName', $bookStores['storeName'])->first();
             if (!$bookStroeDB) {
                 BookStore::create([
@@ -28,7 +28,7 @@ class ImportBookStoreService
                 ]);
             }
 
-            $bookStroeDB = BookStore::where('storeName', $bookStores['storeName'])->first();
+            $bookStroeDB = BookStore::where('storeName', $bookStores['storeName'])->first();    #若bookStore剛剛建立 需要再次讀取
             foreach ($bookStores['books'] as $book) {
                 $bookDB = Books::where('bookName', $book['bookName'])->first();
                 if (!$bookDB) {
@@ -43,6 +43,44 @@ class ImportBookStoreService
                         'price' => $book['price'],
                         'bookStore_id' => $bookStroeDB->id,
                     ]);
+                }
+            }
+        }
+        ##
+        $bookStores = BookStore::select('storeName', 'openingHours')->get();
+        foreach ($bookStores as $bookStore) {
+            $explodeArray = explode('/', $bookStore->openingHours);
+            foreach ($explodeArray as  $explodeString) {
+                $openWeeks = [];
+                foreach ($weekArray as $WeekKey => $week) {
+                    if (strpos($explodeString, $week) !== false) {
+                        array_push($openWeeks, $WeekKey);
+                    }
+                }
+                $explodeBySpeceArray = explode(' ', $explodeString);
+                if (count($openWeeks) == 2 && array_search('-', $explodeBySpeceArray) == 1) {
+                    for ($i = $openWeeks[0] + 1; $i < $openWeeks[1]; $i++) {
+                        array_push($openWeeks, $i);
+                    }
+                }
+                $timeArray = app('OutputTimeService')->outPutTime($explodeString);
+                // dd($openWeeks);
+                // dd($weekArray[$openWeeks[0]], $timeArray);
+                // dd($openTimeDB);
+                foreach ($openWeeks as $openWeek) {
+                    $openTimeDB = BookStoreOpenTime::where('storeName', $bookStore->storeName)->first();
+                    if ($openTimeDB == null) {
+                        BookStoreOpenTime::create([
+                            "storeName" => $bookStore->storeName,
+                            "openTime$weekArray[$openWeek]" => $timeArray[0],
+                            "closeTime$weekArray[$openWeek]" => $timeArray[1],
+                        ]);
+                    } else {
+                        $openTimeDB->update([
+                            "openTime$weekArray[$openWeek]" => $timeArray[0],
+                            "closeTime$weekArray[$openWeek]" => $timeArray[1],
+                        ]);
+                    }
                 }
             }
         }
